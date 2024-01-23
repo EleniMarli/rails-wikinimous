db7cac36bba2de34c40e8cf83c40a22c3234a4c7
Eleni Marli <elenimarli@yahoo.gr>
2024-01-23 17:06:35 +0100

    Generate Article model, run create table migration, seed db, generate controller, use resources for routes and start working on index view for 'Rails Wikinimous' challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+10.times do
+  Article.create(title: Faker::TvShows::RuPaul.unique.queen, content: Faker::Quote.unique.famous_last_words)
+end
